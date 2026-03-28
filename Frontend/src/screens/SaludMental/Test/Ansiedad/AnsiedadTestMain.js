b668345fb2f61fc35344ff9cc4aae40568bf0405
SebastianGonzalezMorales <sebastian.gonzalez@alumnos.uv.cl>
2026-03-28 19:10:06 -0300

    Corrigiendo error que ocurría al abrir el botón Fuente en Test GAD-7

--- Frontend/src/screens/SaludMental/Test/Ansiedad/AnsiedadTestMain.js
+++ Frontend/src/screens/SaludMental/Test/Ansiedad/AnsiedadTestMain.js
@@ -5,7 +5,7 @@ import {
   View,
   Alert,
   TouchableOpacity,
-  StyleSheet, Linkin
+  StyleSheet, Linking
 } from 'react-native';
 import React, { useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
